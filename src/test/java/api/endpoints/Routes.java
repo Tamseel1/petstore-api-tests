c22f3011a3d15a8cf2153759ae68c35a412408cb
Tamseel1 <ahmedtamseels@gmail.com>
2026-06-17 02:20:42 -0400

    first commit

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,30 @@
+package api.endpoints;
+
+public class Routes {
+	
+	
+	public static String base_url = "https://petstore.swagger.io/v2"; 
+	
+	//User Model
+	
+	public static String post_url = base_url + "/user";
+	public static String get_url = base_url + "/user/{username}";
+	public static String update_url = base_url + "/user/{username}";
+	public static String delete_url = base_url + "/user/{username}";
+	
+	
+	
+	// Store Module
+	
+	// here we will create store module url
+	
+	
+	
+	
+	
+	// Pet Module
+	
+	// here we will create pet module url
+	
+	
+}
